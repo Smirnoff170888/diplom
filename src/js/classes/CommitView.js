58dc865785819868d9359fb130738eb4a26f07cc
Смирнова Ирина <freeda170888@yandex.ru>
2019-10-17 03:20:16 +0300

    Change author to commiter on CommitView

--- src/js/classes/CommitView.js
+++ src/js/classes/CommitView.js
@@ -12,12 +12,12 @@ export default class CommitView {
     createDom() {
         const template = `
                 <div class="slider__card">
-                    <p class="slider__date">${Utils.formateDate(this._data.commit.author.date)}</p>
+                    <p class="slider__date">${Utils.formateDate(this._data.commit.committer.date)}</p>
                     <div class="slider__info">
                         <img src="${this._data.author.avatar_url}" alt="Аватар комментатора" class="slider__avatar">
                         <div class="slider__user">
-                            <h3 class="slider__name">${this._data.commit.author.name}</h3>
-                            <p class="slider__email">${this._data.commit.author.email}</p>
+                            <h3 class="slider__name">${this._data.commit.committer.name}</h3>
+                            <p class="slider__email">${this._data.commit.committer.email}</p>
                         </div>
                     </div>
                     <p class="slider__text">${this._data.commit.message}</p>
