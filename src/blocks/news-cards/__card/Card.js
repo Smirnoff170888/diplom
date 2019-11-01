f51a1c9bdd85581cebc5c6a28dde01bde3b9e6a9
Смирнова Ирина <freeda170888@yandex.ru>
2019-11-01 19:40:19 +0300

    Fix news card render

--- src/blocks/news-cards/__card/Card.js
+++ src/blocks/news-cards/__card/Card.js
@@ -21,7 +21,7 @@ export default class NewsCardComponent extends DynamicComponent {
                 <img src="${this._data.urlToImage}" alt="${this._data.title}" class="news-cards__image">
                 <p class="news-cards__date">${Utils.formatDate(this._data.publishedAt)}</p>
                 <h3 class="news-cards__news">${this._data.title}</h3>
-                <p class="news-cards__paragraph">${this._data.content}</p>
+                <p class="news-cards__paragraph">${this._data.description}</p>
             </div>
             <a href="${this._data.url}" target="_blank" class="news-cards__source">${this._data.source.name}</a>`;
         const elem = document.createElement('div');
